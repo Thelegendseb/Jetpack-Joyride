16f9c87a5eebcfb7053c7ccb6635e3eb458fc922
Sebastian Clarke <SebClarke1@outlook.com>
2022-02-23 23:45:09 +0000

    Getting maths involved

--- Obstacle.vb
+++ Obstacle.vb
@@ -3,21 +3,16 @@
     Public Point1 As Point ' |  relative to the game tile
     Public Point2 As Point ' |  ^^^^^^^^^^^^^^^^^^^^^^^^^
 
-
+    'COULD MAKE A BOOL MAP THAT CONNECTS STUFF - MIGHT BE EASIER?
 
     Sub New(CurrentObsList As List(Of Obstacle))
         'need to make sure they are made in relation 
         ' to eachother -- pass in list 
         '-- not too many of each type
 
-        Dim TOB As String = GetTypeOfOb()
-        Dim LineLength As Integer = GetLineLength()
-
         If CurrentObsList.Count > 1 Then
             'here will be checks for overlay
         End If
-
-
 
     End Sub
 
@@ -34,6 +29,38 @@
         Randomize()
         Return Lengths(Int(Rnd() * Lengths.Length - 1))
     End Function
+
+    Private Sub BuildOb()
+
+        Dim TOB As String = GetTypeOfOb()
+        Dim LineLength As Integer = GetLineLength()
+
+        Randomize() 'VALUES NEED TO BE CONSIDERED WHEN OVERLAY CHECK IS DONE
+        Dim GetPointStartY As Integer = (255 / (Int(Rnd() * 10) + 1)) - ((255 / 10) / 2) ' offset
+        Dim GetPointStartX As Integer = (Form1.Width / (Int(Rnd() * 12) + 1)) - ((Form1.Width / 12) / 2)
+
+        Point1 = New Point(GetPointStartX, GetPointStartY) 'going left
+        Select Case TOB
+            Case "H"
+                If Form1.Width / 2 < GetPointStartX Then
+                    Point2 = New Point(GetPointStartX + LineLength, GetPointStartY)
+                Else
+                    Point2 = New Point(GetPointStartX - LineLength, GetPointStartY)
+                End If
+            Case "V"
+                If Form1.Height / 2 < GetPointStartY Then
+                    Point2 = New Point(GetPointStartX, GetPointStartY + LineLength)
+                Else
+                    Point2 = New Point(GetPointStartX, GetPointStartY - LineLength)
+                End If
+            Case "DP"
+                'check here if possible if not possbile relocate
+            Case "DN"
+
+        End Select
+
+    End Sub
+
 
 
 End Class
